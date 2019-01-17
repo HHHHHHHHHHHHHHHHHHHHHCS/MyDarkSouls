efb14849e0eb3faec8c4d078c280ca07ad08d83d
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-01-17 20:09:16 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public bool inputEnable = true;
+    public KeyCode keyUp = KeyCode.W;
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyRight = KeyCode.A;
+    public KeyCode keyLeft = KeyCode.D;
+    public float dUp, dRight,targetDUp,targetDRight,velocityDUp,velocityDRight;
+
+    private void Update()
+    {
+        if (inputEnable)
+        {
+            targetDUp = (Input.GetKey(keyUp) ? 1 : 0) - (Input.GetKey(keyDown) ? -1 : 0);
+            targetDRight = (Input.GetKey(keyRight) ? 1 : 0) - (Input.GetKey(keyLeft) ? -1 : 0);
+
+        }
+        else
+        {
+            targetDUp = 0;
+            targetDRight = 0;
+        }
+
+        dUp = Mathf.SmoothDamp(dUp, targetDUp, ref velocityDUp, 0.1f);
+        dRight = Mathf.SmoothDamp(dRight, targetDRight, ref velocityDRight, 0.1f);
+    }
+}
