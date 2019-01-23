24d8df40a3f8c99744359b45f9c70681931c5e52
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-01-23 20:59:20 +0800

    add anim

--- Assets/Scripts/ActorController.cs
+++ Assets/Scripts/ActorController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class ActorController : MonoBehaviour
 {
+    private readonly int forwardKey = Animator.StringToHash("forward");
+    private readonly int jumpKey = Animator.StringToHash("jump");
+
     public float wakeSpeed = 1.4f;
     public float runMultiplier = 2.75f;
 
@@ -24,10 +27,16 @@ public class ActorController : MonoBehaviour
 
     private void Update()
     {
-        anim.SetFloat("forward", pi.dmag * (pi.isRun ? 2.0f : 1f));
+        anim.SetFloat(forwardKey, pi.dmag * Mathf.Lerp(anim.GetFloat(forwardKey), pi.isRun ? 2.0f : 1f, 0.5f));
+
+        if (pi.jump)
+        {
+            anim.SetTrigger(jumpKey);
+        }
+
         if (pi.dmag >= 0.1f)
         {
-            model.transform.forward = pi.dVec;
+            model.transform.forward = Vector3.Lerp(model.transform.forward, pi.dVec, 0.3f);
         }
 
         movingVec = pi.dmag  * model.transform.forward * wakeSpeed * (pi.isRun ? runMultiplier : 1f);
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [Header("===== Keys Setting====")]
-    public KeyCode keyUp = KeyCode.W;//move up
-    public KeyCode keyDown = KeyCode.S;//move down
-    public KeyCode keyLeft = KeyCode.A;//move left
-    public KeyCode keyRight = KeyCode.D;//move right
-    public KeyCode keyA = KeyCode.LeftShift;//run
-    public KeyCode keyB = KeyCode.S;
+    [Header("===== Keys Setting====")] public KeyCode keyUp = KeyCode.W; //move up
+    public KeyCode keyDown = KeyCode.S; //move down
+    public KeyCode keyLeft = KeyCode.A; //move left
+    public KeyCode keyRight = KeyCode.D; //move right
+    public KeyCode keyA = KeyCode.LeftShift; //run
+    public KeyCode keyB = KeyCode.Space;
     public KeyCode keyC = KeyCode.A;
     public KeyCode keyD = KeyCode.D;
 
-    [Header("===== Out Signals =====")]
-    private float dUp, dRight;
+    [Header("===== Out Signals =====")] private float dUp, dRight;
     public float dmag; //向前的量
     public Vector3 dVec; //旋转的角度
-    public bool isRun;//是否在跑
+    public bool isRun; //是否在跑
+    public bool jump; //是否在跳跃
+    public bool lastJump; //最后的跳跃
 
-    [Header("===== Others =====")]
-    public bool inputEnable = true;
+    [Header("===== Others =====")] public bool inputEnable = true;
 
 
     public float targetDUp, targetDRight, velocityDUp, velocityDRight;
@@ -43,9 +42,36 @@ public class PlayerInput : MonoBehaviour
         dRight = Mathf.SmoothDamp(dRight, targetDRight, ref velocityDRight, 0.1f);
 
 
-        dmag = Mathf.Sqrt(dUp * dUp + dRight * dRight);
-        dVec = (dRight * transform.right + dUp * transform.forward).normalized;
+        Vector2 tempDAxis = SquareToCircle(dRight, dUp);
+        float tempDright = tempDAxis.x;
+        float tempDUp = tempDAxis.y;
+
+        dmag = Mathf.Sqrt(tempDUp * tempDUp + tempDright * tempDright);
+        dVec = tempDright * transform.right + tempDUp * transform.forward;
 
         isRun = Input.GetKey(keyA);
+
+        bool newJump = Input.GetKey(keyB);
+        if (newJump != lastJump && newJump)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+
+        lastJump = newJump;
+    }
+
+    /// <summary>
+    /// 把矩形坐标映射成圆形
+    /// </summary>
+    private Vector2 SquareToCircle(float x, float y)
+    {
+        Vector2 output;
+        output.x = x * Mathf.Sqrt(1 - (y * y) / 2.0f);
+        output.y = y * Mathf.Sqrt(1 - (x * x) / 2.0f);
+        return output;
     }
 }
