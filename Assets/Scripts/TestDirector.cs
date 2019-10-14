595d4bbace43c65ddba52f13d908f8313f205b6e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-10-15 00:13:27 +0800

    add

---
+++ Assets/Scripts/TestDirector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TestDirector : MonoBehaviour
+{
+    public PlayableDirector pd;
+
+    private void Awake()
+    {
+        pd = GetComponent<PlayableDirector>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            pd.Stop();
+            pd.time = 0;
+            pd.Evaluate();//重新计算位置
+            pd.Play();
+        }
+    }
+}
