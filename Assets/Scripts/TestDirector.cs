fd9952007c257e64835bbb4a7573f64d3e9a1085
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-10-17 00:30:29 +0800

    add

--- Assets/Scripts/TestDirector.cs
+++ Assets/Scripts/TestDirector.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.Playables;
+using Debug = UnityEngine.Debug;
 
 public class TestDirector : MonoBehaviour
 {
-    public PlayableDirector pd;
+    public Animator attacker;
+    public Animator victim;
+
+    private PlayableDirector pd;
+
 
     private void Awake()
     {
@@ -17,9 +23,21 @@ public class TestDirector : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.H))
         {
-            pd.Stop();
-            pd.time = 0;
-            pd.Evaluate();//重新计算位置
+            foreach (var track in pd.playableAsset.outputs)
+            {
+                if (track.streamName.Contains("Attacker"))
+                {
+                    pd.SetGenericBinding(track.sourceObject, attacker);
+                }
+                else if (track.streamName.Contains("Victim"))
+                {
+                    pd.SetGenericBinding(track.sourceObject, victim);
+                }
+            }
+
+            //pd.Stop();
+            //pd.time = 0;
+            //pd.Evaluate();//重新计算位置
             pd.Play();
         }
     }
