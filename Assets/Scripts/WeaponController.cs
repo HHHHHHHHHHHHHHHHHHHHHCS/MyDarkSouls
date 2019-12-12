3e45c6198a2ada729aa5ea43c34c294dd072db94
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-12-13 00:30:36 +0800

    temp

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
     public WeaponManager weaponManager;
+    public WeaponData weaponData;
+
+    private void Awake()
+    {
+        weaponData = GetComponentInChildren<WeaponData>();
+    }
 }
