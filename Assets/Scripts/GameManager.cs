b152fd5bed1db8e68c2d7184215fe0d536316590
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-12-18 00:25:45 +0800

    add

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        var prefab = Resources.Load<GameObject>("Falchion");
+    }
+}
