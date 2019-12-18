eb48e520b04fea9500d4ae0aede4800dafe21565
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-12-19 00:23:16 +0800

    add

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+
+    private void Awake()
+    {
+        CheckGameObject();
+    }
+
+    private void CheckSingle()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void CheckGameObject()
+    {
+        if (!gameObject.CompareTag("GM"))
+        {
+            Destroy(this);
+        }
+    }
+
     private void Start()
     {
-        var prefab = Resources.Load<GameObject>("Falchion");
+        //var prefab = Resources.Load<GameObject>("Falchion");
     }
 }
